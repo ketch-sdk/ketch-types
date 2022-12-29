fe5a00bae79d192ab10f0cd22ffd76e6a776de1d
Sam Alexander <35970568+smalexan@users.noreply.github.com>
2022-12-29 14:31:11 -0800

    fix(identity): register identity name (#58)

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ export interface Ketch {
   getConfig(): Promise<Configuration>
 
   registerPlugin(plugin: Plugin, config?: any): Promise<void>
-  registerIdentityProvider(provider: () => Promise<Identities>): Promise<void>
+  registerIdentityProvider(name: string, provider: () => Promise<Identities>): Promise<void>
 
   hasConsent(): boolean
   getConsent(): Promise<Consent>
