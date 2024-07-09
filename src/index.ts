2aa9d17730fdaeeead0a61f2035f5afad30a6387
sergeirybalko <sergeirybalko@gmail.com>
2024-07-09 16:42:53 +0300

    KD-13214 - Added AccessibilityButtonConfiguration interface for ConfigurationV2

--- src/index.ts
+++ src/index.ts
@@ -1185,6 +1185,16 @@ export interface GetPreferenceConfigurationV2Request {
 }
 
 /**
+ * Accessibility Button
+ */
+export interface AccessibilityButtonConfiguration {
+  enabled: boolean
+  color: string
+  icon: string
+  position: 'bottom-left' | 'bottom-right'
+}
+
+/**
  * Configuration V2
  */
 export interface ConfigurationV2 {
@@ -1321,6 +1331,11 @@ export interface ConfigurationV2 {
    * Translations static translations
    */
   translations?: Translations
+
+  /**
+   * Accessibility button configuration
+   */
+  accessibilityButton?: AccessibilityButtonConfiguration
 }
 
 /**
